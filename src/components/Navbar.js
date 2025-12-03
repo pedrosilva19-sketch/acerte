2dd7058b5c95751fa8906f1105acf91b7d5cc68e
pedrosilva19-sketch <pedro.silva.19@academico.ifpb.edu.br>
2025-12-03 01:03:48 -0300

    Initial commit from Create Next App

---
+++ src/components/Navbar.js
@@ -0,0 +1,45 @@
+// components/Navbar.tsx
+import Link from "next/link";
+
+export default function Navbar() {
+    function Home() {
+        window.location.href = "/";
+    }
+
+    return (
+        <nav className="navbar">
+            <div className="navbar-brand">
+                <img
+                    id="logo-nav"
+                    src="/imgs/logo.jpg"
+                    alt="Acerte!"
+                    className="logo-nav"
+                    onClick={Home}
+                />
+                <span id="logo-text">Acerte!</span>
+            </div>
+
+            <ul className="navbar-menu">
+                <li>
+                    <Link href="/">Início</Link>
+                </li>
+                <li>
+                    <Link href="/about">Sobre Nós</Link>
+                </li>
+
+                <li id="auth-item">
+                    {/* Link de Login (visível quando NÃO logado) */}
+                    <Link href="/login" id="login-link">
+                        Login
+                    </Link>
+
+                    {/* Área do usuário logado (visível quando logado) */}
+                    <div className="user-area" id="user-area">
+                        <span id="username-display">Usuário</span>
+                        <button id="logout-btn" className="logout-btn" title="Sair da conta">
+                            <span>Sair</span>
+                        </button>
+                    </div>
+                </li>
+            </ul>
+        </nav>
